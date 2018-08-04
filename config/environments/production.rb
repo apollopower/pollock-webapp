1696f0d17611f3e1f216d01a8ef9af1a2d681856
Jonas Erthal <erthaljonas@gmail.com>
2018-08-04 14:59:34 -0400

    Fixed Typo bug for production ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'https://aqueous-lake-89158.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
