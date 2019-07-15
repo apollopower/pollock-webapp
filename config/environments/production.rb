055b5291af08548d799ab1a1ad26c9c6aed1f600
Thayna Santana <thaynamenezes01@gmail.com>
2019-07-15 15:52:34 -0400

    image working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #img
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
@@ -103,6 +107,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-config.serve_static_assets = true
-config.assets.compile = true
